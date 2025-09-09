763db2556c61c113c047f809b100d763c2791962
ehpc <ehpc@ehpc.io>
2025-09-09 22:18:14 +0300

    Enhance SSH security settings by configuring OpenSSH and enabling fail2ban

--- configuration.nix
+++ configuration.nix
@@ -16,5 +16,19 @@
   systemd.network.enable = true;
 
   # SSH
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      ChallengeResponseAuthentication = false;
+      X11Forwarding = false;
+      AllowAgentForwarding = false;
+      MaxAuthTries = 3;
+      LoginGraceTime = "20s";
+    };
+  };
   services.sshd.enable = true;
+  services.fail2ban.enable = true;
 }
