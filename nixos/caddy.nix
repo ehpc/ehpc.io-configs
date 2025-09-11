ab8196715b32ebd0c3fdd444e66c108c889b2064
ehpc <ehpc@ehpc.io>
2025-09-11 11:16:48 +0000

    Enhance Caddy configuration by adding health check and improving security headers

--- nixos/caddy.nix
+++ nixos/caddy.nix
@@ -4,10 +4,16 @@
     enable = true;
     virtualHosts.${domain}.extraConfig = ''
       encode zstd gzip
+
       @health path /proxy-health
       respond @health "ok" 200
+
       reverse_proxy 127.0.0.1:8080
+
       header {
+        -Server
+        X-Content-Type-Options "nosniff"
+        Referrer-Policy "strict-origin-when-cross-origin"
         Strict-Transport-Security "max-age=31536000; includeSubDomains; preload"
       }
     '';
