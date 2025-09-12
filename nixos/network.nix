fa4aeae9748097638fbd7d1cf0c65c8a78a8c795
ehpc <ehpc@ehpc.io>
2025-09-12 13:42:40 +0000

    Refactor SOPS template for network configuration to improve structure and readability

--- nixos/network.nix
+++ nixos/network.nix
@@ -4,21 +4,29 @@
   networking.useDHCP = false;
   systemd.network.enable = true;
 
-  sops.templates."10-wan.network".content = ''
-    [Match]
-    Name=ens3
+  sops.templates."10-wan.network" = {
+    content = ''
+      [Match]
+      Name=ens3
 
-    [Network]
-    Address=${config.sops.placeholder."ipv4-address"}
-    Address=${config.sops.placeholder."ipv6-address"}
-    Gateway=${config.sops.placeholder."ipv4-gateway"}
+      [Network]
+      Address=${config.sops.placeholder."ipv4-address"}
+      Address=${config.sops.placeholder."ipv6-address"}
+      Gateway=${config.sops.placeholder."ipv4-gateway"}
 
-    [Route]
-    Gateway=${config.sops.placeholder."ipv6-gateway"}
-    GatewayOnLink=true
-  '';
+      [Route]
+      Gateway=${config.sops.placeholder."ipv6-gateway"}
+      GatewayOnLink=true
+    '';
+    owner = "root";
+    group = "root";
+    mode = "0644";
+  };
 
-  environment.etc."systemd/network/10-wan.network".source = config.sops.templates."10-wan.network".path;
+  environment.etc."systemd/network/10-wan.network" = {
+    source = config.sops.templates."10-wan.network".path;
+    mode = "0644";
+  };
 
   systemd.services.systemd-networkd.wants = [
     "sops-nix.service"
