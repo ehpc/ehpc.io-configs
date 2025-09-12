26ae7060ce4cc158f42a1b182feca5c57ec72498
ehpc <ehpc@ehpc.io>
2025-09-12 11:04:28 +0000

    Add SOPS template for network configuration and integrate with systemd-networkd

--- nixos/network.nix
+++ nixos/network.nix
@@ -1,9 +1,28 @@
-{ ... }:
+{ config, ... }:
 {
   networking.useDHCP = false;
   systemd.network.enable = true;
 
-  systemd.network.networks."10-wan" = {
-    matchConfig.Name = "ens3";
-  };
+  sops.templates."10-wan".content = ''
+    [Match]
+    Name=ens3
+
+    [Network]
+    Address=${config.sops.placeholder."ipv4-address"}
+    Address=${config.sops.placeholder."ipv6-address"}
+    Gateway=${config.sops.placeholder."ipv4-gateway"}
+
+    [Route]
+    Gateway=${config.sops.placeholder."ipv6-gateway"}
+    GatewayOnLink=true
+  '';
+
+  environment.etc."systemd/network/10-wan.network".source = config.sops.templates."10-wan".path;
+
+  systemd.services.systemd-networkd.wants = [
+    "sops-nix.service"
+  ];
+  systemd.services.systemd-networkd.after = [
+    "sops-nix.service"
+  ];
 }
