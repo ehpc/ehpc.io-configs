329aed365734fcc5e1617772c36f151c7a1b69c8
ehpc <ehpc@ehpc.io>
2025-09-20 10:18:11 +0000

    Refactor podman-kube service configuration to remove unnecessary empty strings from ExecStart and ExecStop

--- nixos/containers.nix
+++ nixos/containers.nix
@@ -10,17 +10,15 @@
   systemd.timers."podman-auto-update".timerConfig.OnCalendar = "*:0/15";
 
   systemd.services."podman-kube@".serviceConfig = {
-    ExecStart = [
-      ""
-      "/run/current-system/sw/bin/podman kube play --replace --service-container=true /etc/podman/%I.yaml"
+    ExecStart = lib.mkForce [
+      "/run/current-system/sw/bin/podman kube play --replace --service-container=true /etc/podman/%i.yaml"
     ];
-    ExecStop = [
-      ""
-      "/run/current-system/sw/bin/podman kube down /etc/podman/%I.yaml"
+    ExecStop = lib.mkForce [
+      "/run/current-system/sw/bin/podman kube down /etc/podman/%i.yaml"
     ];
   };
 
   environment.etc."podman/ehpc.io.yaml".source = ../web/ehpc.io.yaml;
 
-  systemd.services."podman-kube@ehpc.io.yaml".enable = true;
+  systemd.services."podman-kube@ehpc.io".enable = true;
 }
