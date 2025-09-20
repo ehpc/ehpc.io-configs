434019d2a150c4acf9b8490f02b245fe060faf6e
ehpc <ehpc@ehpc.io>
2025-09-20 09:45:14 +0000

    Add Pod configuration and update Caddyfile for health check and file server

--- nixos/containers.nix
+++ nixos/containers.nix
@@ -7,4 +7,8 @@
       defaultNetwork.settings.dns_enabled = true;
     };
   };
+  systemd.timers."podman-auto-update".timerConfig.OnCalendar = "*:0/15";
+
+  environment.etc."podman/ehpc-io.yaml".source = ../web/ehpc-io.yaml;
+  systemd.services."podman-kube@/etc/podman/ehpc-io.yaml".enable = true;
 }
