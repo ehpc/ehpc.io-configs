06c36ff6eb9964876f7083b8ca7614537b344b25
ehpc <ehpc@ehpc.io>
2025-09-21 13:56:49 +0000

    Update Kubernetes manifest paths to use the parent directory

--- nixos/k3s.nix
+++ nixos/k3s.nix
@@ -9,12 +9,12 @@
       "--write-kubeconfig-group=kube"
       "--write-kubeconfig-mode=644"
     ];
-    manifests.ehpc-io-letsencrypt.source = ./k8s/ehpc-io-letsencrypt.yaml;
-    manifests.ehpc-io-namespace.source = ./k8s/ehpc-io-namespace.yaml;
-    manifests.ehpc-io-ingress.source = ./k8s/ehpc-io-ingress.yaml;
-    manifests.ehpc-io-middleware.source = ./k8s/ehpc-io-middleware.yaml;
-    manifests.ehpc-io-service.source = ./k8s/ehpc-io-service.yaml;
-    manifests.ehpc-io-deployment.source = ./k8s/ehpc-io-deployment.yaml;
-    manifests.ehpc-io-hpa.source = ./k8s/ehpc-io-hpa.yaml;
+    manifests.ehpc-io-letsencrypt.source = ../k8s/ehpc-io-letsencrypt.yaml;
+    manifests.ehpc-io-namespace.source = ../k8s/ehpc-io-namespace.yaml;
+    manifests.ehpc-io-ingress.source = ../k8s/ehpc-io-ingress.yaml;
+    manifests.ehpc-io-middleware.source = ../k8s/ehpc-io-middleware.yaml;
+    manifests.ehpc-io-service.source = ../k8s/ehpc-io-service.yaml;
+    manifests.ehpc-io-deployment.source = ../k8s/ehpc-io-deployment.yaml;
+    manifests.ehpc-io-hpa.source = ../k8s/ehpc-io-hpa.yaml;
   };
 }
