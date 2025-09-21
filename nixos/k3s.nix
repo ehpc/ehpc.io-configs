f7ac4141e90ca67694ad17130743e7afbeea3605
ehpc <ehpc@ehpc.io>
2025-09-21 13:46:52 +0000

    Update Kubernetes manifest paths to use the k8s directory

--- nixos/k3s.nix
+++ nixos/k3s.nix
@@ -11,10 +11,10 @@
     ];
     manifests.ehpc-io-letsencrypt.source = ./k8s/ehpc-io-letsencrypt.yaml;
     manifests.ehpc-io-namespace.source = ./k8s/ehpc-io-namespace.yaml;
-    manifests.ehpc-io-ingress.source = ./web/ehpc-io-ingress.yaml;
-    manifests.ehpc-io-middleware.source = ./web/ehpc-io-middleware.yaml;
-    manifests.ehpc-io-service.source = ./web/ehpc-io-service.yaml;
-    manifests.ehpc-io-deployment.source = ./web/ehpc-io-deployment.yaml;
-    manifests.ehpc-io-hpa.source = ./web/ehpc-io-hpa.yaml;
+    manifests.ehpc-io-ingress.source = ./k8s/ehpc-io-ingress.yaml;
+    manifests.ehpc-io-middleware.source = ./k8s/ehpc-io-middleware.yaml;
+    manifests.ehpc-io-service.source = ./k8s/ehpc-io-service.yaml;
+    manifests.ehpc-io-deployment.source = ./k8s/ehpc-io-deployment.yaml;
+    manifests.ehpc-io-hpa.source = ./k8s/ehpc-io-hpa.yaml;
   };
 }
