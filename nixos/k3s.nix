68b229fdfb9cda487bce1fe1df13b501d539330b
ehpc <ehpc@ehpc.io>
2025-09-21 21:36:47 +0300

    Reorder ehpc-io-ingress manifest source in k3s configuration

--- nixos/k3s.nix
+++ nixos/k3s.nix
@@ -24,8 +24,8 @@
     };
     manifests.ehpc-io-letsencrypt.source = ../k8s/ehpc-io-letsencrypt.yaml;
     manifests.ehpc-io-namespace.source = ../k8s/ehpc-io-namespace.yaml;
-    manifests.ehpc-io-ingress.source = ../k8s/ehpc-io-ingress.yaml;
     manifests.ehpc-io-middleware.source = ../k8s/ehpc-io-middleware.yaml;
+    manifests.ehpc-io-ingress.source = ../k8s/ehpc-io-ingress.yaml;
     manifests.ehpc-io-service.source = ../k8s/ehpc-io-service.yaml;
     manifests.ehpc-io-deployment.source = ../k8s/ehpc-io-deployment.yaml;
     manifests.ehpc-io-hpa.source = ../k8s/ehpc-io-hpa.yaml;
