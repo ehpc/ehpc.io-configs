9a54f95f3f027c298d55ca517c49ea15a274efc4
ehpc <ehpc@ehpc.io>
2025-09-21 13:05:12 +0000

    Add Kubernetes manifests for ehpc-io deployment, service, HPA, ingress, middleware, and letsencrypt

--- nixos/k3s.nix
+++ nixos/k3s.nix
@@ -1,12 +1,20 @@
 { ... }:
 {
   users.groups.kube = { };
-  services.k3s.enable = true;
-  services.k3s.role = "server";
-  services.k3s.extraFlags = toString [
-    "--disable=traefik"
-    "--write-kubeconfig-group=kube"
-    "--write-kubeconfig-mode=644"
-  ];
-  services.k3s.manifests.ehpc-io.source = ../web/ehpc-io-deployment.yaml;
+  services.k3s = {
+    enable = true;
+    role = "server";
+    clusterInit = true;
+    extraFlags = toString [
+      "--write-kubeconfig-group=kube"
+      "--write-kubeconfig-mode=644"
+    ];
+    manifests.ehpc-io-letsencrypt.source = ./k8s/ehpc-io-letsencrypt.yaml;
+    manifests.ehpc-io-namespace.source = ./k8s/ehpc-io-namespace.yaml;
+    manifests.ehpc-io-ingress.source = ./web/ehpc-io-ingress.yaml;
+    manifests.ehpc-io-middleware.source = ./web/ehpc-io-middleware.yaml;
+    manifests.ehpc-io-service.source = ./web/ehpc-io-service.yaml;
+    manifests.ehpc-io-deployment.source = ./web/ehpc-io-deployment.yaml;
+    manifests.ehpc-io-hpa.source = ./web/ehpc-io-hpa.yaml;
+  };
 }
